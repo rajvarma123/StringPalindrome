0f7a98cfda2bf3eacca44df983834e41ed3beb38
raj <rajvarma007009@gmail.com>
2018-11-19 15:59:48 +0530

    Update Program.cs

--- StringPalindrome/StringPalindrome/Program.cs
+++ StringPalindrome/StringPalindrome/Program.cs
@@ -10,6 +10,23 @@ namespace StringPalindrome
     {
         static void Main(string[] args)
         {
+            string s, revs = "";
+            Console.WriteLine(" Enter string");
+            s = Console.ReadLine();
+            for (int i = s.Length - 1; i >= 0; i--) 
+            {
+                revs += s[i].ToString();
+            }
+            if (revs == s)
+            {
+                Console.WriteLine("String is Palindrome \n Entered String Was {0} \n reverse string is {1}", s, revs);
+            }
+            else
+            {
+                Console.WriteLine("String is not Palindrome \n Entered String Was {0} \n reverse string is {1}", s, revs);
+            }
+            Console.ReadKey();
+
         }
     }
 }
