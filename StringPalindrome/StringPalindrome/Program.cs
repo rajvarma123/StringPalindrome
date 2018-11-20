7466d401e12b945d62ba664318ca6d996985aa81
raj <rajvarma007009@gmail.com>
2018-11-20 12:17:11 +0530

    update
    string is valid

--- StringPalindrome/StringPalindrome/Program.cs
+++ StringPalindrome/StringPalindrome/Program.cs
@@ -6,26 +6,40 @@ using System.Threading.Tasks;
 
 namespace StringPalindrome
 {
-    class Program
+   public class Program
     {
+        public void palindrome()
+        {
+            Console.WriteLine("the string is palindrome");
+        }
+        public void invalid()
+        {
+            Console.WriteLine("the string is not palindrome");
+        }
         static void Main(string[] args)
         {
-            string s; 
-     
-            Console.WriteLine(" Enter string");
+            string s;
+           Console.WriteLine(" Enter string");
             s = Console.ReadLine();
+            Program program = new Program();
+            int i = s.Length;
+            int j = 0;
+            while (i > 0)
+            {
+                    if (s[j++] == s[i- 1])
+                    {
+                        i--;
+                    }
+                    else
+                    {
+                        program.invalid();
+                        break;
+                    }
 
-            if (s[0] == s[s.Length - 1] ||s[0+1]==s[s.Length-2])
-                
-            {
-                Console.WriteLine("String is Palindrome");
             }
-            else
-            {
-                Console.WriteLine("String is not palindrome");
-            }
+            program.palindrome();
+
             Console.ReadKey();
-
         }
     }
 }
